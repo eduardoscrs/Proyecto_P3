9cdf2f2234de102b0fed6b90a7814f1f537f8094
Nelson Quiñinao <127786657+Anker04@users.noreply.github.com>
2025-06-03 22:11:43 -0400

    Dependencias y base

---
+++ Proyect/visual/dashboard.py
@@ -0,0 +1,9 @@
+# Proyect/visual/dashboard.py
+import streamlit as st
+
+def main():
+    st.title("Dashboard Principal")
+    st.write("Bienvenido al panel de control.")
+
+if __name__ == "__main__":
+    main()
